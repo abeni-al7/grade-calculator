18442772da32604991f5468ed0935499c9b0afb4
abeni-al7 <abenezeralebachew3@gmail.com>
2025-07-13 23:29:59 +0300

    Refactor input acceptance logic

--- main.go
+++ main.go
@@ -8,8 +8,7 @@ var (
 	subjects = make(map[string]float64)
 )
 
-func main() {
-
+func acceptBasicInfo() {
 	fmt.Println("Welcome to the Student Grade Tracking App")
 	fmt.Println("What is your name?")
 	fmt.Scanln(&name)
@@ -17,18 +16,26 @@ func main() {
 	fmt.Println("Hello,", name)
 	fmt.Println("How many subjects have you taken?")
 	fmt.Scanln(&amountOfSubjects)
+}
+
+func acceptSubject(i int) {
+	var name string
+	fmt.Println("Enter the name of Subject", i + 1)
+	fmt.Scanln(&name)
+
+	var grade float64
+	fmt.Println("What is your grade for", name, "?")
+	fmt.Scanln(&grade)
+
+	subjects[name] = grade
+}
+
+func main() {
+	acceptBasicInfo()
 
 	i := 0
 	for i < amountOfSubjects {
-		var name string
-		fmt.Println("Enter the name of Subject", i)
-		fmt.Scanln(&name)
-
-		var grade float64
-		fmt.Println("What is your grade for", name, "?")
-		fmt.Scanln(&grade)
-
-		subjects[name] = grade
+		acceptSubject(i)
 		i++
 	}
 	fmt.Println(subjects)
